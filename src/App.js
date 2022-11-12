572a459d8d395c2a45856f5094790979af3d900d
Josie Angelica Carrillo <angelica@josies-mbp.attlocal.net>
2022-11-11 16:27:28 -0800

    adding all

---
+++ src/App.js
@@ -0,0 +1,16 @@
+//import logo from './logo.svg'; - add in my personal pic here (:
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> My Awesome Blog
+        <div id="page-body">
+         Welcome to my blog!
+         </div>
+      </h1>
+    </div>
+  );
+}
+
+export default App;

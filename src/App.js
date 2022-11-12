14eb14d8153513433185ed34fb2a8b67085904ad
Josie Angelica Carrillo <angelica@josies-mbp.attlocal.net>
2022-11-11 17:02:06 -0800

    added routing using react router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 //import logo from './logo.svg'; - add in my personal pic here (:
 import './App.css';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ArticlePage from './pages/ArticlePage';
@@ -9,16 +9,20 @@ import ArticlesListPage from './pages/ArticlesListPage';
 
 function App() {
   return (
+  <BrowserRouter>
     <div className="App">
       <h1> My Awesome Blog
         <div id="page-body">
-            <HomePage />
-            <AboutPage />
-            <ArticlesListPage />
-            <ArticlePage />
-         </div>
+           <Routes>
+             <Route path = "/" element= {<HomePage />} />
+             <Route path = "/about" element = {<AboutPage />} />
+             <Route path = "/articles" element = {<ArticlesListPage />} />
+             <Route path = "/articles/:articleId" element = {<ArticlePage /> } />
+           </Routes>
+        </div>
       </h1>
     </div>
+   </BrowserRouter>
   );
 }
 

fd34478884c09de08233f9ee7ebc4547d973f28a
Josie Angelica Carrillo <angelica@josies-mbp.attlocal.net>
2022-11-14 16:21:58 -0800

    added functionality to param

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,10 +1,16 @@
 import { useParams } from 'react-router-dom';
+import articles from './article-content';
 
 const ArticlePage = () => {
 const params = useParams();
 const { articleId } = useParams();
+const article = articles.find(article => article.name === articleId);
     return (
-        <h1>  This is the article page with the id: {articleId} </h1>
+    <>
+    <h1>{article.title}</h1>
+    {article.content.map(paragraph => (
+    <p>{paragraph}</p>))}
+    </>
     );
 }
 

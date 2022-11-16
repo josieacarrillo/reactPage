5249eafe21931d23e004bd1e79357068b6d9567b
Josie Angelica Carrillo <angelica@josies-mbp.attlocal.net>
2022-11-15 16:45:26 -0800

    displaying diff artcles using react link

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -8,8 +8,8 @@ const article = articles.find(article => article.name === articleId);
     return (
     <>
     <h1>{article.title}</h1>
-    {article.content.map(paragraph => (
-    <p>{paragraph}</p>))}
+    {article.content.map((paragraph, i) => (
+    <p key={i}>{paragraph}</p>))}
     </>
     );
 }

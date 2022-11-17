9a5fa77fb41ef55d76c4a560d1694243b79dcb65
Josie Angelica Carrillo <josieacarrillo@gmail.com>
2022-11-16 16:30:42 -0800

    added upvotes

--- src/server.js
+++ src/server.js
@@ -1,17 +1,33 @@
 import express from 'express';
 
+let articlesInfo = [{
+    name: 'learn-react',
+    upvotes: 0,
+    }, {
+    name: 'learn-node',
+    upvotes: 0,
+
+    }, {
+    name: 'mangodb',
+    upvotes: 0,
+
+    }]
+
 const app = express ();
 app.use(express.json());
 
-app.post('/hello', (req, res) => {
-    res.send(`Hello, ${req.body.name}!`);
+app.put('/api/articles/:name/upvote', (req,res) => {
+    const { name } = req.params;
+    const article = articlesInfo.find( a => a.name === name);
+   if (article){
+    article.upvotes+=1;
+
+    res.send(`The ${name} article now has ${article.upvotes} upvotes`)
+    } else {
+    res.send(`That article doesn\t exist`)
+    }
 });
 
-app.get('/hello/:name', (req,res) => {
-    const { name } = req.params;
-    res.send(`Hello ${name}!!`)
-}
-)
 app.listen(8000, () => {
     console.log('Server is listening on port 8000');
 });

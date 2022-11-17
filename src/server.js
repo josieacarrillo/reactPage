71c4866161904f43e631d5ccf672a77def55b3d9
Josie Angelica Carrillo <josieacarrillo@gmail.com>
2022-11-16 17:22:41 -0800

    basic server using nodejs

--- src/server.js
+++ src/server.js
@@ -3,14 +3,15 @@ import express from 'express';
 let articlesInfo = [{
     name: 'learn-react',
     upvotes: 0,
+    comments: [],
     }, {
     name: 'learn-node',
     upvotes: 0,
-
+    comments: [],
     }, {
     name: 'mangodb',
     upvotes: 0,
-
+    comments: [],
     }]
 
 const app = express ();
@@ -22,9 +23,23 @@ app.put('/api/articles/:name/upvote', (req,res) => {
    if (article){
     article.upvotes+=1;
 
-    res.send(`The ${name} article now has ${article.upvotes} upvotes`)
+    res.send(`The ${name} article now has ${article.upvotes} upvotes!!!`)
     } else {
     res.send(`That article doesn\t exist`)
+    }
+});
+
+app.post('/api/articles/:name/comments', (req, res) => {
+    const {name} = req.params;
+    const { postedBy, text } = req.body;
+
+    const article = articlesInfo.find(a => a.name === name);
+
+    if (article){
+        article.comments.push({ postedBy, text});
+        res.send(article.comments);
+    } else {
+        res.send("that article doesnt exist")
     }
 });
 
